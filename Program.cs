b6a21bdc82da61adde36a85cab1491fbb3a2772b
AnnisaDmynt221 <97298883+AnnisaDmynt221@users.noreply.github.com>
2022-03-23 21:15:07 +0700

    Add files via upload
    TaxiApp

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+            Taxi taxi1 = new Taxi();
+
+
+            taxi.DriverName = "Fajar";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 18;
+
+            taxi1.DriverName = "Aditya";
+            taxi1.OnDuty = false;
+            taxi1.NumPassenger = 15;
+
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            taxi1.TaxiInfo();
+            taxi1.PickUpPassenger();
+            taxi1.DropOffPassenger();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+        public string Keterangan { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("***********************************");
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            if(OnDuty == true)
+            {
+                Keterangan = "Yes";
+            }
+            else
+            {
+                Keterangan = "No";
+            }
+            Console.WriteLine("OnDuty: {0}", Keterangan);
+            Console.WriteLine("Num Passenger: {0}\n", NumPassenger);
+            Console.WriteLine("-----------------------------------");
+
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang\n", DriverName);
+            Console.WriteLine("***********************************");
+        }
+    }
+}
